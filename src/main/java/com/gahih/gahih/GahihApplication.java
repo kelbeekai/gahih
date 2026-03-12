8507e679663346c94afa514b839a490830ccbebd
Kelbee_Kai <ridmwk123@gmail.com>
2026-03-13 03:08:51 +0900

    Initial commit

---
+++ src/main/java/com/gahih/gahih/GahihApplication.java
@@ -0,0 +1,13 @@
+package com.gahih.gahih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GahihApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GahihApplication.class, args);
+	}
+
+}
